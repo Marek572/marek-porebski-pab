1a9c92ce0cb20cd614af4c69c21b50b746bbafef
Marek Porębski <marek.bp@poczta.fm>
2022-03-09 09:42:06 +0100

    projekt1

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express()
 app.get('/', function (req, res) { res.send('Hello World') })
 
 //calc
-//app.get('/calculator', (req, res) => { const {operation, num1 ,num2} = req.query })
+// app.get('/add', (req, res) => { const {num1 ,num2} = res.send(this.num1 + this.num2)})
 
 
 app.listen(3001)
